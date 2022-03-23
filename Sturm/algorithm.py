e911734d8dbd35a46bd7be7aa548c7dba8616855
Stiven <zono-capensis@protonmail.com>
2022-03-22 21:27:24 -0500

    Get fully STURM Sequence

--- Sturm/algorithm.py
+++ Sturm/algorithm.py
@@ -14,17 +14,35 @@ for i in range(len(list_of_coefficients)):
     list_of_coefficients[i]=functions.string_to_int(list_of_coefficients[i])
 
 poly = Polynomial(degree, list_of_coefficients)
-print(poly)
+#print(poly)
 
 interval = poly.find_root_interval()
-print(interval)
+#print(interval)
 
 derivative = functions.find_derivative(poly)
-print(derivative)
+#print(derivative)
 
 p2 = functions.synth_division(poly.list_of_coefficients, derivative.list_of_coefficients)
-print(p2)
+#print(p2)
 
+dividend = poly
+divisor = derivative
+sturm_sequence = [poly,derivative]
+is_not_finished = True
+actual_degree = derivative.degree -1
+while is_not_finished:
+    remainder = functions.synth_division(dividend.list_of_coefficients, divisor.list_of_coefficients)
+    temp_poly = Polynomial(degree=actual_degree,list_of_coefficients=remainder)
+    print(temp_poly)
+    actual_degree-=1
+    sturm_sequence.append(temp_poly)
+    dividend = divisor
+    divisor = temp_poly
+    if len(remainder)<1:
+        is_not_finished = False
+
+for i in sturm_sequence:
+    print(i)
 
 
 
