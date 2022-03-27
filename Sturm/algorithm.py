0794e8038ed7e6d34d69385cac9874938465f435
Stiven <zono-capensis@protonmail.com>
2022-03-27 14:12:54 -0500

    Edit while loop and correct Sturm sequence

--- Sturm/algorithm.py
+++ Sturm/algorithm.py
@@ -13,38 +13,23 @@ list_of_coefficients = cli.get_coefficients()
 for i in range(len(list_of_coefficients)):
     list_of_coefficients[i]=functions.string_to_int(list_of_coefficients[i])
 
-poly = Polynomial(degree, list_of_coefficients)
-#print(poly)
+dividend= Polynomial(degree, list_of_coefficients)
+divisor = functions.find_derivative(dividend)
 
-interval = poly.find_root_interval()
-#print(interval)
+coefficients_of_remainder = functions.synth_division(dividend.list_of_coefficients, divisor.list_of_coefficients)
+current_degree = divisor.degree - 1
+remainder = Polynomial(current_degree,coefficients_of_remainder)
 
-derivative = functions.find_derivative(poly)
-#print(derivative)
+sturm_sequence = [dividend,divisor,remainder]
 
-p2 = functions.synth_division(poly.list_of_coefficients, derivative.list_of_coefficients)
-#print(p2)
-
-dividend = poly
-divisor = derivative
-sturm_sequence = [poly,derivative]
-is_not_finished = True
-actual_degree = derivative.degree -1
-while is_not_finished:
-    remainder = functions.synth_division(dividend.list_of_coefficients, divisor.list_of_coefficients)
-    temp_poly = Polynomial(degree=actual_degree,list_of_coefficients=remainder)
-    print(temp_poly)
-    actual_degree-=1
-    sturm_sequence.append(temp_poly)
+while current_degree > 0:
     dividend = divisor
-    divisor = temp_poly
-    if len(remainder)<1:
-        is_not_finished = False
+    divisor = remainder
+    
+    coefficients_of_remainder = functions.synth_division(dividend.list_of_coefficients, divisor.list_of_coefficients)
+    current_degree = divisor.degree - 1
+    remainder = Polynomial(current_degree,coefficients_of_remainder)
+    sturm_sequence.append(remainder)
 
-for i in sturm_sequence:
-    print(i)
-
-
-
-
-
+for poly in sturm_sequence:
+    print(poly)
